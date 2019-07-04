968cb1aca33c35981c588b6f8b54e1c84d6207a0
theProgrammerDavid <davidvelho2awesome@gmail.com>
2019-07-04 17:14:20 +0530

    checking for updates

--- Windows/tools/install.bat
+++ Windows/tools/install.bat
@@ -4,15 +4,16 @@ set CHECK_UPDATE=''
 
 IF exist %temp%\.CSI-WebApp-Template (CALL :fn_dir_exists) ELSE (CALL :fn_dir_not_exist)
 
-set /p CHECK_UPDATE="Do you want to check for updates? (yes/no) :"
-IF "%CHECK_UPDATE%"=="yes" (CALL :fn_check_updates)
 
 EXIT /B %ERRORLEVEL%
 
 
 :fn_dir_exists
 set CHECK_UPDATE='yes'
-echo its here
+echo CSI-WebApp-Template is already installed.
+set /p CHECK_UPDATE="Do you want to check for updates? (y/n) :"
+IF "%CHECK_UPDATE%"=="y" (CALL :fn_check_updates)
+
 EXIT /B 0
 
 :fn_dir_not_exist
